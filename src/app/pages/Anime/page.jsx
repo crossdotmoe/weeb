25e7be1f3dcd81e2844df11536a36f2c14b839d8
cross <github@cross.moe>
2024-09-16 07:37:20 +0530

    init

---
+++ src/app/pages/Anime/page.jsx
@@ -0,0 +1,57 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import BigCarousel from "@/components/BigCarousel";
+import { FetchAniwatchHomePage } from "../../../hooks/useApi.jsx";
+import ReusableCarousel from "@/components/ReusableCarousel";
+import ReusableStack from "@/components/ReusableStack";
+import ReusableCardStacks from "@/components/ReusableCardStacks";
+import EstimatedSchedule from "@/components/EstimatedSchedule.jsx";
+import TopAnimesTable from "@/components/TopAnimesTable.jsx";
+import ContinueWatching from '@/components/continue-watching-section.jsx';
+import { extractData, fetchHomePage } from '@/hooks/ApiMapper.jsx';
+const page = () => {
+  const [data, setData] = useState(null);
+  const [TrendingData, setTrendingData] = useState(null);
+  const [popularData, setPopularData] = useState(null);
+  const [top10AnimesData, setTop10AnimesData] = useState(null);
+  const [upcomingAnimesData, setUpcomingAnimesData] = useState(null);
+  const [tableData, setTableData] = useState(null);
+  const [cardStackData, setCardStackData] = useState(null);
+  const [genreData, setGenreData] = useState(null);
+  useEffect(() => {
+    const loadData = async () => {
+      const data = await fetchHomePage();
+      setData(data.spotlightAnimes);
+      setTop10AnimesData(data.top10Animes);
+      setTrendingData(data.top10Animes.today);
+      setPopularData(data.top10Animes.month);
+      setTableData(data.topAiringAnimes);
+      setCardStackData(data.latestEpisodeAnimes);
+      setGenreData(data.genres);
+      setUpcomingAnimesData(data.topUpcomingAnimes);
+    };
+    loadData();
+  }, []);
+
+
+  return (
+    <div className="flex flex-col px-10 gap-10 max-md:px-2 bg-custom">
+      <BigCarousel data={data || []} />
+      <ContinueWatching />
+      <ReusableCarousel title={"Trending"} data={TrendingData || []} />
+      <ReusableCarousel title={"Popular"} data={popularData || []} />
+      <TopAnimesTable data={top10AnimesData || []} />
+      <ReusableStack data={tableData || []} />
+      <ReusableCardStacks title={'Upcoming Animes'} data={upcomingAnimesData || []} withGenres={false} />
+      <ReusableCardStacks
+        withGenres={true}
+        genresData={genreData || []}
+        title={"Latest Episodes"}
+        data={cardStackData || []}
+      />
+      <EstimatedSchedule />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/app/pages/Manga/page.jsx
@@ -0,0 +1,39 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { FetchMangaList } from "@/hooks/useApi";
+import HomeCarousel from "@/components/Manga/HomeCarousel";
+import ReusableCarousel from "@/components/Manga/ReusableCarousel";
+import MangaTable from "@/components/Manga/MangaTable";
+import ReusableStack from "@/components/Manga/ReusableCardStacks";
+import BackgroundOverlay from '@/components/common/BackgroundOverlay';
+const Manga = () => {
+  const [mangaList, setMangaList] = useState(null);
+  const [newestData, setNewestData] = useState(null);
+  const [latestData, setLatestData] = useState(null);
+  const [tableData, setTableData] = useState(null);
+  useEffect(() => {
+    const loadData = async () => {
+      let TableData = [];
+      const data = await FetchMangaList(1);
+      const NewData = await FetchMangaList(2);
+      setMangaList(data.mangaList.slice(0, 11));
+      setNewestData(NewData.mangaList.slice(11, 23));
+      setLatestData(data.mangaList.slice(12, 23));
+      TableData.push(data.mangaList.slice(0,10), data.mangaList.slice(11,21), NewData.mangaList.slice(0,10), NewData.mangaList.slice(11, 21));
+      setTableData(TableData);
+    };
+    loadData();
+  }, []);
+
+  return (
+    <div className="flex flex-col gap-10 px-10 max-md:px-2 bg-custom">
+      <HomeCarousel data={mangaList} />
+      <ReusableCarousel title={"Trending"} data={newestData} />
+      <ReusableCarousel title={"Popular"} data={latestData} />
+      <ReusableStack data={tableData} title={'Top Reads'} />
+      <MangaTable data={tableData} />
+    </div>
+  );
+};
+
+export default Manga;
